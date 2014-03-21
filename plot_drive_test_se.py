2efdef8b12077149b929919c1cbf846d8ef4458d
Ruben Merz <ruben.merz@ieee.org>
2014-03-21 23:43:02 +0100

    Change plot legend location
    Signed-off-by: Ruben Merz <ruben.merz@ieee.org>

--- plot_drive_test_se.py
+++ plot_drive_test_se.py
@@ -79,6 +79,7 @@ def main(args):
                        'Spectral efficiency (PRB norm.)',
                        'Spectral efficiency',
                        'bit/s/Hz')
+    plt.legend(loc=0,prop={'size':10})
     plt.subplot2grid((2,2), (1,0),colspan=1)
     dpl.plot_density(df['SE'].replace([np.inf, -np.inf], np.nan).dropna(),x,
                      'Spectral efficiency','bit/s/Hz')
@@ -97,6 +98,7 @@ def main(args):
                           'Spectral efficiency (PRB norm.)\n',
                           'Spectral efficiency 15 MHz (PRB norm.)',
                           'bit/s/Hz')
+    plt.legend(loc=0,prop={'size':10})
     if args.print:
         plt.savefig(args.print[1],dpi=300,bbox_inches='tight')
 
