695cbc75618399ece0165d17b88a730fa570b3d6
Ruben Merz <ruben.merz@ieee.org>
2014-03-27 15:41:58 +0100

    Add scripts to plot MCS for QPSK and 16QAM
    Signed-off-by: Ruben Merz <ruben.merz@ieee.org>

---
+++ plot_drive_test_mcs_qpsk.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+import sys, argparse
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+import numpy as np
+import pandas as pd
+
+from matplotlib import pyplot as plt
+
+from drive_test_analysis import trace_loader as tl
+from drive_test_analysis import nemo_trace_processor as ntp
+from drive_test_analysis import data_plotter as dpl
+
+def setup_args():
+    parser = argparse.ArgumentParser(description='Plot drive test data.')
+    parser.add_argument('-l','--list', action='store_true', help='List all data-set.')
+    parser.add_argument('-s','--static', action='store_true', help='Keep samples with zero velocity.')
+    parser.add_argument('-d','--select', type=int,
+                        help='Select a particular data-set to display')
+    parser.add_argument('library',type=str, nargs='+',
+                        help='Select a particular library to pull data from')
+    parser.add_argument('--print', type=str, help='Print figure to file.')
+    args   = parser.parse_args()
+    return args
+
+def main(args):
+    data_file_list = tl.get_data_file_list(args.library)
+    if args.list:
+        tl.print_list(data_file_list)
+        sys.exit(0)
+    data = tl.load_data_file(data_file_list,args.select)
+
+    if not args.static:
+        logging.debug('Remove zero velocity samples')
+        data = ntp.remove_non_positive_velocity_samples(data)
+
+    ntp.process_data(data,ntp.process_velocity)
+    ntp.process_data(data,ntp.process_pdsch_mcs_qpsk)
+
+    column_list = ['mcs_0','mcs_1','mcs_2','mcs_3','mcs_4','mcs_5','mcs_6','mcs_7','mcs_8','mcs_9']
+
+    if args.select is None:
+        df = tl.concat_pandas_data([df[column_list] for df in data ])
+    else:
+        df = data[column_list]
+
+    plt.ion()
+    plt.figure()
+
+    for index in np.arange(0,10):
+        print(index,index)
+        print(df[['mcs_'+str(index)]].dropna().describe())
+        plt.boxplot(df[['mcs_'+str(index)]].dropna().values,positions=[index+1])
+    plt.ylim([0,100])
+    plt.xlim([0,11])
+    plt.xticks(np.arange(1,11),['MCS '+str(index) for index in np.arange(0,10)],rotation=45)
+    plt.grid(True)
+    plt.tight_layout()
+
+    if args.print:
+        plt.savefig(args.print,dpi=300,bbox_inches='tight')
+
+    input('press any key')
+
+if __name__ == "__main__":
+    args = setup_args()
+    main(args)
